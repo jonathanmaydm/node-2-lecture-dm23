a4ad4a4806c756708c3216f6294a4b97b669e49b
Jonathan May <jonathan.may@devmountain.com>
2019-05-20 12:57:08 -0500

    Add lecture code

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,9 +1,39 @@
-const students = [];
+const axios = require("axios");
+let students = [];
+
+// IF you want data from an outside source, you can
+// use axios on the server too. It's still just making
+// an HTTP request, but this time it's from one server to another.
+
+// axios.get("http://breakingbadapi.com/api/characters").then(response => {
+//   students = response.data;
+// });
 
 const getStudents = (req, res) => {
   res.json(students);
 };
 
+const addStudent = (req, res) => {
+  // if (req.body.name === "Jonathan") {
+  //   res.status(500).json("Not today.");
+  // } else {
+
+  // we ONLY have access to req.body because we used
+  // app.use(express.json()) in index.js
+  students.push(req.body);
+  res.json(students);
+  // }
+};
+
+const updateStudent = (req, res) => {
+  // we can work with all this data just like
+  // we normally do in Javascript
+  students[+req.params.id].name = req.body.name;
+  res.json(students);
+};
+
 module.exports = {
-  getStudents
+  getStudents,
+  addStudent,
+  updateStudent
 };
--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const studentController = require("./controllers/studentController");
 
 const app = express();
 
-app.get("/api/users", userController.getUsers);
-app.get("/api/users/:id", userController.getUser);
-app.get("/api/students", studentController.getStudents);
+// our own custom middleware
+// app.use((req, res, next) => {
+//   console.log("Somone accessed the server", req.ip, req.url);
+//   next();
+// });
+
+// middleware that express gives us for free
+// order matters! it has to come before the endpoints
+app.use(express.json());
+
+/* Chris */ app.get("/api/users", userController.getUsers);
+/* Bryan */ app.get("/api/users/:id", userController.getUser);
+/* Kevin */ app.get("/api/students", studentController.getStudents);
+app.post("/api/students", studentController.addStudent);
+app.put("/api/students/:id", studentController.updateStudent);
 
 const PORT = 5050;
 app.listen(PORT, () => {
